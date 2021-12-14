19d3b83604bc0b8ecfacc2b5ce3dc893751b80fc
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2021-12-14 13:28:40 -0800

    useEffect moved to provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import Layout from './views/Layout/Layout'
 import Home from './views/Home/Home'
-import { fetchUser } from './services/user'
-import { useUser } from './context/UserContext'
 
-function App() {
-  const {setUser} = useUser();
-
-  useEffect(() => {
-    fetchUser()
-      .then((fetchedUser) => {
-        setUser(fetchedUser)
-      })
-      .catch((error) => {
-        throw new Error(`Error: ${error}`)
-      })
-  }, [setUser])
+function App() { 
 
   return (
     <Layout>
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,14 +1,28 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { fetchUser } from '../services/user';
 
 const UserCtx = createContext();
 
 const UserProvider = ({children}) => {
-    const [user, setUser] = useState({})
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        fetchUser()
+          .then((fetchedUser) => {
+            setUser(fetchedUser)
+          })
+          .catch((error) => {
+            throw new Error(`Error: ${error}`)
+          })
+      }, []);
+
     return <UserCtx.Provider value={{ user, setUser }}>{children}</UserCtx.Provider>
 }
 
 const useUser = () => {
     const ctx = useContext(UserCtx);
+
+    
 
     if (ctx === undefined) {
         throw new Error('useUser can only be called as a child of UserContext Provider')
